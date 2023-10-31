d6fb6b0858020600aeaebf5b4fb24ddccaba414e
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-10-31 19:39:12 -0400

    Add tile coloring on hover

--- etchASketch.js
+++ etchASketch.js
@@ -2,6 +2,14 @@ const tileBoard = document.querySelector('.tileBoard');
 window.addEventListener('load', () => {
     createTileBoard(16);
 });
+
+tileBoard.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    if(target.classList.contains('tile')) {
+        target.classList.toggle("tileBlack");
+    }
+});
+
 function createTileBoard(amount) {
     for(let i = 0; i < amount; i++) {
         let tileRow = document.createElement('div');
