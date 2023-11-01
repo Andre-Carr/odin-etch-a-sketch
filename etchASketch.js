5f80814349f3437fd83d376ec657753458cc33f9
Andre Carr <129695312+Andre-Carr@users.noreply.github.com>
2023-11-01 01:49:52 -0400

    Add progressive darkening when the mouse hovers over a tile

--- etchASketch.js
+++ etchASketch.js
@@ -6,7 +6,37 @@ window.addEventListener('load', () => {
 tileBoard.addEventListener('mouseover', (event) => {
     let target = event.target;
     if(target.classList.contains('tile')) {
-        target.classList.add("tileBlack");
+        if(target.classList.contains('tileBlackZero')) {
+            target.classList.remove('tileBlackZero')
+            target.classList.add("tileBlackOne");
+        } else if(target.classList.contains('tileBlackOne')) {
+            target.classList.remove('tileBlackOne');
+            target.classList.add("tileBlackTwo");
+        } else if(target.classList.contains('tileBlackTwo')) {
+            target.classList.remove('tileBlackTwo');
+            target.classList.add("tileBlackThree");
+        } else if(target.classList.contains('tileBlackThree')) {
+            target.classList.remove('tileBlackThree');
+            target.classList.add("tileBlackFour");
+        } else if(target.classList.contains('tileBlackFour')) {
+            target.classList.remove('tileBlackFour');
+            target.classList.add("tileBlackFive");
+        } else if(target.classList.contains('tileBlackFive')) {
+            target.classList.remove('tileBlackFive');
+            target.classList.add("tileBlackSix");
+        } else if(target.classList.contains('tileBlackSix')) {
+            target.classList.remove('tileBlackSix');
+            target.classList.add("tileBlackSeven");
+        } else if(target.classList.contains('tileBlackSeven')) {
+            target.classList.remove('tileBlackSeven');
+            target.classList.add("tileBlackEight");
+        } else if(target.classList.contains('tileBlackEight')) {
+            target.classList.remove('tileBlackEight');
+            target.classList.add("tileBlackNine");
+        } else if(target.classList.contains('tileBlackNine')) {
+            target.classList.remove('tileBlackNine');
+            target.classList.add("tileBlackTen");
+        }
     }
 });
 
@@ -29,7 +59,8 @@ function createTileBoard(amount) {
         tileRow.setAttribute('class', 'tileRow');
         for(let j = 0; j < amount; j++) {
             let tile = document.createElement('div');
-            tile.setAttribute('class', 'tile');
+            tile.classList.add('tile');
+            tile.classList.add('tileBlackZero');
             tileRow.appendChild(tile);
         }
         tileBoard.appendChild(tileRow);
@@ -39,6 +70,17 @@ function createTileBoard(amount) {
 function clearTiles() {
     const tileList = document.querySelectorAll('.tile');
     tileList.forEach((tile)=> {
-        tile.classList.remove('tileBlack');
+        tile.classList.remove('tileBlackOne');
+        tile.classList.remove('tileBlackTwo');
+        tile.classList.remove('tileBlackThree');
+        tile.classList.remove('tileBlackFour');
+        tile.classList.remove('tileBlackFive');
+        tile.classList.remove('tileBlackSix');
+        tile.classList.remove('tileBlackSeven');
+        tile.classList.remove('tileBlackEight');
+        tile.classList.remove('tileBlackNine');
+        tile.classList.remove('tileBlackTen');
+        
+        tile.classList.add("tileBlackZero");
     });
 }
